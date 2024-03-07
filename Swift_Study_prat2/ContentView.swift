65859715b44390e4f2de3de9e41c2289bd11f56b
seomini <seomini970204@gmail.com>
2024-03-07 12:34:44 +0900

    Initial Commit

---
+++ Swift_Study_prat2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift_Study_prat2
+//
+//  Created by mimi_0_0 on 2024/03/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift_Study_prat2/Swift_Study_prat2App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Study_prat2App.swift
+//  Swift_Study_prat2
+//
+//  Created by mimi_0_0 on 2024/03/07.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Study_prat2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
